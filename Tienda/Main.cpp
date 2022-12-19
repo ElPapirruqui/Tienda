f752884ad1cbc02c8381a74dece36baeffc9d266
Ignacio Urrutia <ignaciourrutiamichelini@gmail.com>
2022-12-19 11:58:48 -0300

    Added base files for project

---
+++ Tienda/Main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "Bienvenido al campo de entrenamiento soldado bla bla bla...\n" << endl;
+    cout << "¿Que desea hacer?" << endl;
+    cout << "(Seleccionar opcion)\n" << endl;
+    cout << "1 - Recoger arma" << endl;
+    cout << "2 - Dejar arma" << endl;
+    cout << "3 - Disparar" << endl;
+    cout << "4 - Ver arma en uso" << endl;
+    cout << "0 - Salir" << endl;
+
+    return 0;
+}
